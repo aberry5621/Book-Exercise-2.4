2f858ecc217e2393c4244666ae1a55137708a32b
alexberry721 <alexberry721@gmail.com>
2016-08-29 16:37:51 -0700

    Init comit

--- Book Exercise 2.4/main.cpp
+++ Book Exercise 2.4/main.cpp
@@ -5,11 +5,34 @@
 //  Created by ax on 8/25/16.
 //  Copyright © 2016 COMP130. All rights reserved.
 //
+//  Convert pounds into kilograms
+//
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // Program Title
+    cout << "Pound to Kilograms Conversion\n";
+    
+    // data storage
+    double input_pound = 0.0;
+    double calc_kilo = 0.0;
+    
+    // I
+    // read input numbers as pounds
+    cout << "Enter a value in pounds: ";
+    cin >> input_pound;
+    
+    // P
+    // convert input to kilos
+    // 1 pound = 0.454 kilograms
+    calc_kilo = input_pound * 0.454;
+    
+    // O
+    // output result in kilograms
+    cout << input_pound << " pounds is " << calc_kilo << " kilograms" << endl;
+    
     return 0;
 }
